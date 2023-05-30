8e909bd1862eac91d87c3fc8459c5a6e687b02d9
oliver <N0937135@my.ntu.ac.uk>
2023-05-30 22:06:49 +0100

    new

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var fadeInElements = document.querySelectorAll('.fade-in');
-    var delay = 500; // Adjust the delay time (in milliseconds) between each item
+    var delay = 250; // Adjust the delay time (in milliseconds) between each item
 
     for (var i = 0; i < fadeInElements.length; i++) {
         var element = fadeInElements[i];
@@ -14,3 +14,27 @@ window.addEventListener('scroll', function() {
         }
     }
 });
+window.addEventListener('DOMContentLoaded', function() {
+    var fadeLoadElements = document.querySelectorAll('.fade-load');
+    var delay = 200; // Adjust the delay time (in milliseconds) between each item
+
+    for (var i = 0; i < fadeLoadElements.length; i++) {
+        setTimeout(function(item) {
+            item.classList.add('visible');
+        }, i * delay, fadeLoadElements[i]);
+    }
+});
+
+window.addEventListener('load', function() {
+    var slideInElements = document.querySelectorAll('.slide-in');
+    var delay = 200; // Adjust the delay time (in milliseconds) between each item
+
+    for (var i = 0; i < slideInElements.length; i++) {
+        var element = slideInElements[i];
+
+        setTimeout(function(item) {
+            item.classList.add('visible');
+        }, i * delay, element);
+    }
+});
+
