bbd5372374f72684184d8282b9c93b079f5a8212
oliver <N0937135@my.ntu.ac.uk>
2023-06-01 21:57:12 +0100

    new

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-window.addEventListener('scroll', function() {
-    var fadeInElements = document.querySelectorAll('.fade-in');
-    var delay = 250; // Adjust the delay time (in milliseconds) between each item
+var fadeInElements = document.querySelectorAll('.fade-in');
+var delay = 250; // Adjust the initial delay time (in milliseconds) between each item
+var lastScrollTime = Date.now();
+var lastScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+var currentDelay = delay;
+
+function fadeInElement(element, delay) {
+    setTimeout(function(item) {
+        item.classList.add('visible');
+    }, delay, element);
+}
+
+function calculateDelay(scrollVelocity) {
+    // Adjust the formula to calculate the new delay based on the scroll velocity
+    // For example, you can divide the initial delay by the scroll velocity
+    return delay / scrollVelocity;
+}
+
+function scrollHandler() {
+    var currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollVelocity = Math.abs(currentScrollPosition - lastScrollPosition) / (Date.now() - lastScrollTime);
+
+    if (scrollVelocity > 0) {
+        currentDelay = calculateDelay(scrollVelocity);
+    }
 
     for (var i = 0; i < fadeInElements.length; i++) {
         var element = fadeInElements[i];
@@ -8,12 +30,18 @@ window.addEventListener('scroll', function() {
         var windowHeight = window.innerHeight;
 
         if (position < windowHeight) {
-            setTimeout(function(item) {
-                item.classList.add('visible');
-            }, i * delay, element);
+            fadeInElement(element, i * currentDelay);
         }
     }
+
+    lastScrollTime = Date.now();
+    lastScrollPosition = currentScrollPosition;
+}
+
+window.addEventListener('scroll', function() {
+    requestAnimationFrame(scrollHandler);
 });
+
 window.addEventListener('DOMContentLoaded', function() {
     var fadeLoadElements = document.querySelectorAll('.fade-load');
     var delay = 200; // Adjust the delay time (in milliseconds) between each item
